f16c58467e6a14e9475499a19291e0c65159db83
Francesco Battaglini <francesco@battaglini.net>
2021-02-24 22:44:04 +1100

    print buf

--- index.js
+++ index.js
@@ -143,6 +143,7 @@ function getModel() {
     if (verbose) {
         log(`Getting model...`);
         const buf = Buffer.from(`WRD${0x12}${0x4d}\n`, 'ascii');
+        console.log(buf);
         vpro.write(buf, (err) => {
             if (err) {
                 logError(`Failed to get firmware version`);
